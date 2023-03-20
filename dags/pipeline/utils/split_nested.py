2d77be4c96e70ba29b0023a44fcf1b6abe6956b1
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2023-03-20 13:38:08 +0700

    feat: split nested column

--- dags/pipeline/utils/split_nested.py
+++ dags/pipeline/utils/split_nested.py
@@ -3,7 +3,7 @@ import json
 
 def split_nested(ti):
     data = ti.xcom_pull(key="data", task_ids="get_detail")
-    contact, location, sha, places = [], [], [], []
+    contact, location, sha, facility, service, places = [], [], [], [], [], []
     for place in data:
         place_obj = json.loads(place)
 
@@ -19,9 +19,19 @@ def split_nested(ti):
         tmp_contact['place_id'] = place_obj['place_id']
         contact.append(tmp_contact)
 
+        tmp_facility = place_obj.pop('facilities')
+        tmp_facility['place_id'] = place_obj['place_id']
+        facility.append(tmp_facility)
+
+        tmp_service = place_obj.pop('services')
+        tmp_service['place_id'] = place_obj['place_id']
+        service.append(tmp_service)
+
         places.append(place_obj)
     
     ti.xcom_push(key="data_contact", value=contact)
     ti.xcom_push(key="data_location", value=location)
     ti.xcom_push(key="data_sha", value=sha)
+    ti.xcom_push(key="data_facility", value=facility)
+    ti.xcom_push(key="data_service", value=service)
     ti.xcom_push(key="data_places", value=places)
