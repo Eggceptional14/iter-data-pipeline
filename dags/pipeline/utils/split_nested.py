19cd273a52bc64584e1831864b9f499e93a5d3e4
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2023-03-26 14:02:13 +0700

    refactor: refactor split nested code

--- dags/pipeline/utils/split_nested.py
+++ dags/pipeline/utils/split_nested.py
@@ -1,37 +1,42 @@
 import json
+import pandas as pd
 
 
 def split_nested(ti):
     data = ti.xcom_pull(key="data", task_ids="get_detail")
-    contact, location, sha, facility, service, places = [], [], [], [], [], []
-    for place in data:
-        place_obj = json.loads(place)
-
-        tmp_location = place_obj.pop('location')
-        tmp_location['place_id'] = place_obj['place_id']
-        location.append(tmp_location)
-
-        tmp_sha = place_obj.pop('sha')
-        tmp_sha['place_id'] = place_obj['place_id']
-        sha.append(tmp_sha)
-
-        tmp_contact = place_obj.pop('contact')
-        tmp_contact['place_id'] = place_obj['place_id']
-        contact.append(tmp_contact)
-
-        tmp_facility = place_obj.pop('facilities')
-        tmp_facility['place_id'] = place_obj['place_id']
-        facility.append(tmp_facility)
-
-        tmp_service = place_obj.pop('services')
-        tmp_service['place_id'] = place_obj['place_id']
-        service.append(tmp_service)
-
-        places.append(place_obj)
     
-    ti.xcom_push(key="data_contact", value=contact)
-    ti.xcom_push(key="data_location", value=location)
-    ti.xcom_push(key="data_sha", value=sha)
-    ti.xcom_push(key="data_facility", value=facility)
-    ti.xcom_push(key="data_service", value=service)
-    ti.xcom_push(key="data_places", value=places)
+    data_casted = []
+    for d in data:
+        data_casted.append(json.loads(d))
+    place_df = pd.DataFrame.from_records(data_casted)
+
+    location = pd.json_normalize(place_df.location)
+    location['place_id'] = place_df['place_id']
+    out_location = location.to_json(orient='records')
+
+    contact = pd.json_normalize(place_df.contact)
+    contact['place_id'] = place_df['place_id']
+    print(contact)
+    out_contact = contact.to_json(orient='records')
+
+    sha = pd.json_normalize(place_df.sha)
+    sha['place_id'] = place_df['place_id']
+    out_sha = sha.to_json(orient='records')
+
+    facility = pd.json_normalize(place_df.facilities)
+    facility['place_id'] = place_df['place_id']
+    out_facility = facility.to_json(orient='records')
+
+    service = pd.json_normalize(place_df.services)
+    service['place_id'] = place_df['place_id']
+    out_service = service.to_json(orient='records')
+
+    place_df.drop(columns=['location', 'contact', 'sha', 'facilities', 'services'], inplace=True)
+    out_place = place_df.to_json(orient='records')
+
+    ti.xcom_push(key="data_contact", value=out_contact)
+    ti.xcom_push(key="data_location", value=out_location)
+    ti.xcom_push(key="data_sha", value=out_sha)
+    ti.xcom_push(key="data_facility", value=out_facility)
+    ti.xcom_push(key="data_service", value=out_service)
+    ti.xcom_push(key="data_places", value=out_place)
