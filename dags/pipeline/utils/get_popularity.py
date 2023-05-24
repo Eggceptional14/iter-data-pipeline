fe1ef6315caf570f29d2af71132f34f30e09dd7a
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2023-05-24 17:57:36 +0700

    refactor: remove unused code

--- dags/pipeline/utils/get_popularity.py
+++ dags/pipeline/utils/get_popularity.py
@@ -28,37 +28,26 @@ def get_popularity(place_name, destination):
     results_num = ''.join([num for num in total_results_text if num.isdigit()])
     return int(results_num)
 
-def exit_handler(df, pop, utf):
-    df['popularity'] = pop
-    temp_df = df[['place_id','place_name','destination','popularity']]
- 
-    temp_df.to_csv('temp/place_popularity4.csv', index=False)
-    with open('temp/unable_to_find_pop3.txt', mode='wt') as file:
-        for i in utf:
-            file.write(str(i) + '\n')
-
 def create_popularity(ti):
     popularity = []
-    unable_to_find = []
     data = ti.xcom_pull(key="raw_data", task_ids="get_raw_data")
-    df = pd.read_json(data, orient='records')
+    df = pd.DataFrame(data)
 
-    # df = pd.read_csv('temp/place_popularity.csv')
-    # df = pd.read_csv('temp/place.csv.gz', compression='gzip')
     # popularity = df['popularity'].to_list()
-    for ind, row in df.iloc[11561:].iterrows():
+    for ind, row in df.iterrows():
         time.sleep(0.2)
         pop = get_popularity(row['place_name'], row['destination'])
         if pop == None:
             popularity.append(0)
-            unable_to_find.append(ind)
         else:
-            popularity[ind] = pop
-
+            # popularity[ind] = pop
+            popularity.append(pop)
+            
+        # if ind == 499:
+        #     break
+    
+    # df = df.head(500)
     df['popularity'] = popularity
     columns = ['place_id','place_name','destination','popularity']
-    # df.to_csv('temp/place_popularity.csv')
+    print(df[columns])
     ti.xcom_push(key="data_popularity", value=df[columns].to_json(orient='records'))
-    # with open('temp/unable_to_find_pop.txt', mode='wt') as file:
-    #     for i in unable_to_find:
-    #         file.write(str(i) + '\n')
